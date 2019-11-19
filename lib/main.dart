c1498494c594cfab6665ab3b6b7baa645ba1520c
Adrian Mullally <gadgetboyme@gmail.com>
2019-11-19 16:31:55 +0000

    initial build

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  @override
+  @override    
+  
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
